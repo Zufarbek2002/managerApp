caa45ccefce26d531f9ce4aa97dc0cce1d5da2c8
Zufarbek <game123qwerty00@gmail.com>
2025-01-12 11:58:18 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      primaryColor: "#2C3030",
+      secondaryColor: '#9A9C9C',
+      darkGreen: "#14B890",
+      darkRed: "#F44336",
+      lightGreen: "#52C41A",
+      lightRed: "#FF8A00",
+      textDarkGrey: "#4E4E4E",
+      paginationGreen: "#14B890",
+      sidebarBgColor: '#F3F3F3'
+    }
   },
   plugins: [],
 }
