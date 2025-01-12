2aebbfff3b7519cdb4c412318a56f2ad7b2cb003
jaloliddinshamsiddinov <125132177+jaloliddinshamsiddinov@users.noreply.github.com>
2025-01-12 12:44:41 +0500

    adding folder/menuitem

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,23 @@
 import LogoI from "../../assets/logo";
+import { menuItem } from "../../constants/menuItem";
 import Details from "../details/Details";
 
 const Home = () => {
+  
   return <div className="flex ">
     <div className="sidebar w-[230px] h-screen bg-[#F3F3F3] border-r-[2px] border-[#E8E8E8] p-[16px]">
       <div>
         <a href="#">
           <LogoI></LogoI>
         </a>
+      </div>
+      <div>
+        {menuItem.map((item,index)=> (
+          <div key={index}>
+            <p>{item.name}</p>
+            <img src={item.icon} className="text-primaryColor" alt="asdf" />
+          </div>
+        ))}
       </div>
     </div>
 
@@ -18,7 +28,7 @@ const Home = () => {
         </div>
       </div>
       <div className="content py-[28px] px-[24px]">
-        <Details></Details>
+        <Details />
       </div>
     </div>
   </div>;
