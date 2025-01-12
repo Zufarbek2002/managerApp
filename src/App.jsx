aa6d41893e883378e713060cbb0ab699bfaa891f
Zufarbek <game123qwerty00@gmail.com>
2025-01-12 11:00:23 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 const App = () => {
+  const token = localStorage.getItem("token");
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      {token ? (
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      ) : (
+        <Login />
+      )}
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
