88c8123a887cb55bc34b6ed9d389c1d48742eaca
Zufarbek <game123qwerty00@gmail.com>
2025-01-13 12:02:07 +0500

    added employee page

---
+++ src/services/getEmployee.jsx
@@ -0,0 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import { customAxios } from "../hooks/customAxios";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+
+const getEmployee = () => {
+  const { token } = useContext(AuthContext);
+
+  const { data = [] } = useQuery({
+    queryKey: ["employees"],
+    queryFn: () =>
+      customAxios
+        .get("/employees", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          return res.data.map((m, index) => {
+            return {
+              ...m,
+              key: index,
+              phone: "+7 900 000-00-00",
+            };
+          });
+        })
+        .catch((e) => console.log(e)),
+  });
+
+  return { data };
+};
+
+export default getEmployee;
